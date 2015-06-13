110020ae10b511f4ab4feed45dcb6d1be5f50c56
Edilson Gonzalez <edilson.gonza@unitec.edu>
2015-06-13 01:10:05 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
+var app = express();
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+//----------------------------------------------------------------------------------------------------------------
+
+mongoose.connect('mongodb://localhost/data');
+
+var UserSchema = mongoose.Schema({
+  username: String,
+  password: String
+});
+
+var User = mongoose.model('User', UserSchema);
+
+//----------------------------------------------------------------------------------------------------------------
+
+app.get('/get_data', function(req, res) {
+  User.find(function(err, users) {
+    if (err) 
+      console.log(err);
+    else
+      res.json(users);
+  });
+});
+
+app.post('/save_data', function(req, res) {
+  var user = new User({ username: req.body.username, password: req.body.password });
+  user.save(function(err) {
+    if (err) {
+      console.log(err);
+      render.json({ state: "error" });
+    } else {
+      console.log("Almacenada con éxito!");
+    }
+  });
+  
+  res.json({ state: "ok" });
+});
+
+var server = app.listen(3000);
